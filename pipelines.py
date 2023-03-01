a1cb504d282d274a994aa971353970991028810f
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-03-01 13:45:32 +0100

    bug: can't find torch module

---
+++ pipelines.py
@@ -0,0 +1,81 @@
+import itertools
+import logging
+from typing import Optional, Dict, Union
+
+from nltk import sent_tokenize
+
+import torch
+from transformers import(
+    AutoModelForSeq2SeqLM, 
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+
+class QGAR:
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer
+    ):
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def __call__(self, context: str):
+        inputs = self._prepare_inputs_for_e2e_qg(context)
+
+        outs = self.model.generate(
+            input_ids=inputs['input_ids'].to(self.device),
+            attention_mask=inputs['attention_mask'].to(self.device),
+        )
+
+        prediction = self.tokenizer.decode(outs[0], skip_special_tokens=True)
+        questions = prediction.split("<sep>")
+        questions = [question.strip() for question in questions[:-1]]
+        return questions
+
+    def _prepare_inputs_for_e2e_qg(self, context):
+        source_text = f"generate questions: {context} </s>"
+        inputs = self._tokenize([source_text], padding=False)
+        return inputs
+    
+    def _tokenize(
+        self,
+        inputs,
+        padding=True,
+        truncation=True,
+        add_special_tokens=True,
+        max_length=512
+    ):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs,
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt"
+        )
+        return inputs
+
+SUPPORTED_TASKS = {
+    "e2e-qg": {
+        "impl": QGAR,
+        "default": {
+            "model": "valhalla/t5-base-e2e-qg",
+        }
+    }
+}
+
+MODEL = "valhalla/t5-base-e2e-qg"
+
+def pipeline(
+    # model: Optional = None,
+    tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+):
+    
+    tokenizer = AutoTokenizer.from_pretrianed(tokenizer)
+    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL)
+
+    return QGAR(model=model, tokenizer=tokenizer)
