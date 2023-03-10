2e4252b6fecaceaa33c9975ae7a503a1022ce8b9
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-10 14:01:38 +0100

    chor(arguments.py): Formatted code

--- arguments.py
+++ arguments.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass, field
 
+
 @dataclass
 class ModelArguments:
     model_name: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+        metadata={
+            "help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
 
     model_type: str = field(
         metadata={"help": "t5 or bart"}
     )
+
 
 @dataclass
 class DataTrainingArguments:
