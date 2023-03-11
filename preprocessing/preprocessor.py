ed26aa224e6c01db9d0fbb361d57aeb579bbb850
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-11 17:24:07 +0100

    feat: Added logging and fix deprecated fields issues

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -1,4 +1,3 @@
-from transformers import T5TokenizerFast, T5ForConditionalGeneration
 from datasets import load_dataset
 import torch
 import logging
@@ -8,12 +7,12 @@ _MAX_TARGET_LENGTH = 64
 _EOS_TOKEN = "</s>"
 _SEP_TOKEN = "<sep>"
 
-logger = logging.getLogger("Preprocessor")
+logger = logging.getLogger(__name__)
 
 class Preprocessor:
-    def __init__(self, model_name: str):
-        self._model: T5ForConditionalGeneration = T5ForConditionalGeneration.from_pretrained(model_name)
-        self._tokenizer: T5TokenizerFast = T5TokenizerFast.from_pretrained(model_name)
+    def __init__(self, model, tokenizer):
+        self._model = model
+        self._tokenizer = tokenizer
         
     def preprocess_dataset(self):
         logger.warning("Downloading SQuAD dataset...")
@@ -27,9 +26,11 @@ class Preprocessor:
         self._model.resize_token_embeddings(len(self._tokenizer))
 
         # For each dataset entry point add eos, <sep>, and encodings
+        logger.warning("Preprocessing SQuAD dataset...")
         tokenized_dataset = raw_dataset.map(self._add_eos_tokens)
         tokenized_dataset = tokenized_dataset.map(self._add_sep_tokens)
         tokenized_dataset = tokenized_dataset.map(self._create_encodings, batched=True)
+        logger.warning("Done.")
         
         # Remove columns 'context' and 'questions'
         tokenized_dataset = tokenized_dataset.remove_columns(["context", "questions"])
@@ -44,30 +45,30 @@ class Preprocessor:
         validation_dataset.set_format(type='torch', columns=columns)
 
         # Save split dataset to disk in PyTorch format
-        logger.warning("Saving processed dataset to './data/'...")
+        logger.warning("Saving processed dataset to directory './data/'...")
         torch.save(training_dataset, './data/training_data.pt')
         torch.save(validation_dataset, './data/validation_data.pt')
         logger.warning("Successfully saved processed datasets.")
 
     def _create_encodings(self, example_batch):
         input_encodings = self._tokenizer.batch_encode_plus(
-            example_batch['context'],
-            max_length=_MAX_INPUT_LENGTH,
+            example_batch['context'], 
+            max_length=_MAX_INPUT_LENGTH, 
             add_special_tokens=True,
-            truncation=True,
-            padding=True
+            truncation=True, 
+            padding='max_length'
         )
         
         target_encodings = self._tokenizer.batch_encode_plus(
-            example_batch['questions'],
-            max_length=_MAX_TARGET_LENGTH,
+            example_batch['questions'], 
+            max_length=_MAX_TARGET_LENGTH, 
             add_special_tokens=True,
-            truncation=True,
-            padding=True
+            truncation=True, 
+            padding='max_length'
         )
-        
+
         encodings = {
-            'input_ids': input_encodings['input_ids'],
+            'input_ids': input_encodings['input_ids'], 
             'attention_mask': input_encodings['attention_mask'],
             'decoder_input_ids': target_encodings['input_ids'],
             'decoder_attention_mask': target_encodings['attention_mask']
