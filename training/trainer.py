ed5520a2e45f729b30371ad8ffc12bf6a24518b6
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-12 16:30:30 +0100

    chor: Cleaned up code

--- training/trainer.py
+++ training/trainer.py
@@ -1,11 +1,9 @@
 import torch
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, PreTrainedModel
 from preprocessing.data_collator import T2TDataCollator
 
-_MODEL_SAVE_PATH = "./models/"
-
 class QGARTrainer:
-    def __init__(self, model, training_file: str, validation_file: str, training_arguments: TrainingArguments):
+    def __init__(self, model: PreTrainedModel, training_file: str, validation_file: str, training_arguments: TrainingArguments):
         """
         Initializes an instance of QGARTrainer for the given model. 
         The provided model will be trained on the specified training and validation file for the given training arguments.
@@ -27,15 +25,3 @@ class QGARTrainer:
         """Start training the model."""
 
         self._trainer.train()
-
-    def save(self, path: str = _MODEL_SAVE_PATH):
-        """Save the trained model to the specified directory path."""
-
-        self._trainer.save_model(path)
-
-    def train_and_save(self, path: str = _MODEL_SAVE_PATH):
-        """Start training the model. Once completed the trained model will be saved to the specified directory path."""
-
-        self.train()
-        self.save(path)
-        
