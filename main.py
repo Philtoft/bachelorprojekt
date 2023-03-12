dcea42f15cf1f7b0d3cda62f62c54689e24c1c92
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-12 16:29:48 +0100

    feat: Implemented runnable main

--- main.py
+++ main.py
@@ -1,52 +1,119 @@
+import argparse
+import json
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+import torch
+import logging
 from arguments import ModelArguments, DataTrainingArguments
 from transformers import (
     HfArgumentParser, 
-    TrainingArguments, 
-    T5Tokenizer, 
-    BartTokenizer, 
-    T5TokenizerFast,
-    T5ForConditionalGeneration
+    TrainingArguments,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer
 )
 from training.trainer import QGARTrainer
+from preprocessing.preprocessor import Preprocessor
+from huggingface_hub import login
 
-MODEL_TYPE_TO_TOKENIZER = {
-    "t5": T5Tokenizer,
-    "bart": BartTokenizer,
-}
 
-def main():
-    model_args, data_args, training_args = parse_args()
+_SETTINGS = "./settings.json"
+_TOKEN_PATH = ".local/token.txt"
 
-    if (
-        os.path.exists(training_args.output_dir)
-        and os.listdir(training_args.output_dir)
-        and training_args.do_train
-        and not training_args.overwrite_output_dir
-    ):
-        raise ValueError(
-            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
-        )
+logger = logging.getLogger(__name__)
 
-    model = T5ForConditionalGeneration.from_pretrained(
-        pretrained_model_name_or_path=model_args.model_name
+def main(args: argparse.Namespace):
+    # Log into Huggingface Hub
+    login(get_hg_token(), add_to_git_credential=True)
+
+    # Parse settings.json
+    model_args, data_args, training_args = parse_settings()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    if args.train:
+        logger.warning("--- Training ---")
+        train_model(model_args.model_name, device, data_args, training_args)
+        exit()
+    elif args.input:
+        logger.warning("--- Question Generation ---")
+        run_model(model_args.model_name, device, args.input)
+        exit()
+    else:
+        parser.print_help()
+
+def run_model(model_name: str, device: str, input_text: str):
+    model, tokenizer = load_model_and_tokenizer(model_name, device)
+
+    print(f"Input: '{input_text}'")
+
+    generator_args = {
+        "max_length": 256,
+        "num_beams": 4,
+        "length_penalty": 1.5,
+        "no_repeat_ngram_size": 3,
+        "early_stopping": True,
+    }
+
+    input_string = "generate questions: " + input_text + " </s>"
+    input_ids = tokenizer.encode(input_string, return_tensors="pt")
+    res = model.generate(input_ids, **generator_args)
+    output = tokenizer.batch_decode(res, skip_special_tokens=True)
+    output = [item.split("<sep>") for item in output]
+
+    print(output)
+
+def train_model(model_name: str, device: str, data_args: DataTrainingArguments, training_args: TrainingArguments):
+    """
+    Trains the specified model on the provided datasets using the specified training arguments.
+    If the datasets are not present locally, they will be redownloaded and preprocesed.
+    """
+    model, tokenizer = load_model_and_tokenizer(model_name, device)
+
+    # Check if datasets are available
+    if (not os.path.exists(data_args.training_file_path) or not os.path.exists(data_args.validation_file_path)):
+        logger.warning("Datasets not present in './data/'.")
+        create_datasets(model, tokenizer)
+    
+    # Initialize trainer
+    trainer = QGARTrainer(
+        model, 
+        data_args.training_file_path, 
+        data_args.training_file_path, 
+        training_args
     )
 
-    tokenizer = T5TokenizerFast.from_pretrained(model_args.model_name)
+    trainer.train()
 
-    # Initialize trainer
-    trainer = QGARTrainer(model, data_args, training_args, tokenizer)
-    trainer.setup()
-    trainer.train_and_save()
+def create_datasets(model, tokenizer):
+    """Downloads and preprocesses the SQuAD dataset for model training."""
 
-def parse_args() -> tuple[ModelArguments, DataTrainingArguments, TrainingArguments]:
-    with open("./settings.json", "r", encoding="utf-8") as file:
-        print(file.read())
+    preprocessor = Preprocessor(model, tokenizer)
+    preprocessor.preprocess_dataset()
 
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
-    return parser.parse_json_file("./settings.json")
+def get_hg_token() -> str:
+    with open(_TOKEN_PATH, "r") as file:
+        return file.read()
+
+def load_model_and_tokenizer(model_name: str, device: str) -> tuple[PreTrainedModel, PreTrainedTokenizer]:
+    # Load model and tokenizer
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)
+
+    return model, tokenizer
+
+def parse_settings() -> tuple[ModelArguments, DataTrainingArguments, TrainingArguments]:
+    """Parses the model, data and training arguments for the given model specified in 'settings.json'."""
+
+    with open(_SETTINGS, "r", encoding="utf-8") as file:
+        settings = json.load(file)
+        mergedEntries = settings['model_arguments'] | settings['data_training_arguments'] | settings['training_arguments']
+
+        parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+        return parser.parse_dict(mergedEntries)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog='QGAR', description="Run or train the QGAR model.")
+    parser.add_argument("-t", "--train", action='store_true', help="Specify that the model should be trained.")
+    parser.add_argument("-i", "--input", type=str, metavar="text", help="Input text to the model.")
+
+    main(parser.parse_args())
