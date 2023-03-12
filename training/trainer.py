0744cc1dab58e9566d9f4bc0479b1741b819f665
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-03-12 21:13:22 +0100

    feat: Made trainer save tokenizer

--- training/trainer.py
+++ training/trainer.py
@@ -1,9 +1,9 @@
 import torch
-from transformers import Trainer, TrainingArguments, PreTrainedModel
+from transformers import Trainer, TrainingArguments, PreTrainedModel, PreTrainedTokenizer
 from preprocessing.data_collator import T2TDataCollator
 
 class QGARTrainer:
-    def __init__(self, model: PreTrainedModel, training_file: str, validation_file: str, training_arguments: TrainingArguments):
+    def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, training_file: str, validation_file: str, training_arguments: TrainingArguments):
         """
         Initializes an instance of QGARTrainer for the given model. 
         The provided model will be trained on the specified training and validation file for the given training arguments.
@@ -13,6 +13,7 @@ class QGARTrainer:
         train = torch.load(training_file)
         validation = torch.load(validation_file)
 
+        self._tokenizer = tokenizer
         self._trainer = Trainer(
             model=model,
             args=training_arguments,
@@ -25,3 +26,8 @@ class QGARTrainer:
         """Start training the model."""
 
         self._trainer.train()
+
+        if self._trainer.is_world_process_zero():
+            self._tokenizer.push_to_hub("the-coorporation/t5-qgar")
+
+        self._trainer.push_to_hub("the-coorporation/t5-qgar")
