45f4581b36c70ea038c14e14e0114d2b72daf950
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-04-07 10:36:36 +0200

    feat gets notes

---
+++ qgar.py
@@ -0,0 +1,23 @@
+
+
+
+class qgar():
+    """
+    This class has the following tasks:
+    - Parses and cleans students notes
+    - Inserts the notes into the QG model
+
+    """
+
+    def __init__(self, notes: str):
+        self.notes = notes
+
+    def parse_notes():
+        # 1) Get notes from file
+        with open("./data/notes/thhs.md") as fp:
+            print(fp.read())
+
+
+qgar("thhs.md")
+
+qgar.parse_notes()
