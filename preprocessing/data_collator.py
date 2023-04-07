3e1b802b641a99f65e78fc452a1c3d4b70781469
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-04-07 14:43:01 +0200

    chor: Cleaned up docs

--- preprocessing/data_collator.py
+++ preprocessing/data_collator.py
@@ -6,8 +6,7 @@ from transformers.utils import PaddingStrategy, TensorType
 @dataclass
 class T2TDataCollator(DataCollatorForSeq2Seq):
     """
-    A data collator for a text-to-text transformer model, which dynamically pads a batch to the longest
-    example length in the batch.
+    A data collator for a text-to-text transformer model, which by default dynamically pads a batch to the longest length of the batch.
     """
 
     def __init__(self, model: AutoModelForSeq2SeqLM, tokenizer: T5TokenizerFast, padding: PaddingStrategy = PaddingStrategy.LONGEST):
