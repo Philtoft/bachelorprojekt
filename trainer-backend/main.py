d7fa211fcacb2f59f42639a267971e6cab8d141b
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-04-13 09:57:12 +0200

    feat: gets wiki article

--- trainer-backend/main.py
+++ trainer-backend/main.py
@@ -1,7 +1,9 @@
 import json
 from flask import Flask, request, Response
 from flask_cors import CORS
+import requests
 import wikiscraper as ws
+from bs4 import BeautifulSoup as bs4
 
 ws.lang("en")
 
@@ -10,20 +12,27 @@ CORS(app)
 
 @app.route('/')
 def index():
-    slug = request.args.get('url')
+    wikiURL = request.args.get('url')
 
-    if slug is None:
+    if wikiURL is None:
         return Response('{"error": "No URL provided"}', status=400, mimetype='application/json')
     
     # Check if article exists
-    wiki_article_abstract = get_wiki_data(slug)
-    article_merged = merge_abstract_sentences(wiki_article_abstract)
+    wiki_article_scraped = get_wiki_data(wikiURL)
     
-    return json.dumps({ 'article': article_merged })
+    return json.dumps({ 'article': wiki_article_scraped })
 
-def get_wiki_data(slug):
-    ws_result = ws.searchBySlug(slug)
-    return ws_result.getAbstract()
+def get_wiki_data(url):
+    
+    response = requests.get(url=url)
+
+    soup = bs4(response.content, 'html.parser')
+
+    title = soup.find(id="firstHeading")
+
+    print(title.string)
+
+    return "result"
     
 if __name__ == '__main__':
     app.run(debug=True)
--- trainer-ui/src/App.tsx
+++ trainer-ui/src/App.tsx
@@ -6,14 +6,14 @@ import './App.css'
 
 function App() {
 
-  const [slug, setSlug] = useState('Artificial_intelligence')
+  const [wikiURL, setWikiURL] = useState('https://en.wikipedia.org/wiki/Artificial_intelligence')
   const [text, setText] = useState('')
 
   function handleSubmit() {
     axios.get('http://127.0.0.1:5000/',
       {
         headers: { "Access-Control-Allow-Origin": "*" },
-        params: { url: slug }
+        params: { url: wikiURL }
       }
     )
       .then((res) => {
@@ -28,9 +28,9 @@ function App() {
   return (
     <div className="App">
       <div style={{ flexDirection: 'column', display: 'flex', width: 500 }}>
-        <h1>Scraper SLUG</h1>
+        <h1>Scraper URL</h1>
         {/* input URL */}
-        <input type="text" style={{ padding: 15 }} onChange={(e) => setSlug(e.target.value)} value={slug} />
+        <input type="text" style={{ padding: 15 }} onChange={(e) => setWikiURL(e.target.value)} value={wikiURL} />
         {/* button */}
         <button style={{ marginTop: '10px' }} onClick={handleSubmit}>Submit</button>
         <p>{JSON.stringify(text)}</p>
