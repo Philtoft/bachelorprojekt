2d85df06ed5b7c3d7f277b15270c13fcb8543d57
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-04-13 09:28:05 +0200

    gets and returns the abstract

--- trainer-backend/main.py
+++ trainer-backend/main.py
@@ -1,15 +1,30 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
+import wikiscraper as ws
 
+ws.lang("en")
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    url = request.args.get('url')
-    return json.dumps({ 'URL': url })
+    slug = request.args.get('url')
+
+    if slug is None:
+        return Response('{"error": "No URL provided"}', status=400, mimetype='application/json')
+    
+    # Check if article exists
+    wiki_article_abstract = get_wiki_data(slug)
+    
+    return json.dumps({ 'article': wiki_article_abstract })
+
+def get_wiki_data(url):
+    ws_result = ws.searchBySlug(url)
+    print(ws_result)
+    return ws_result.getAbstract()
+    
 
                        
 if __name__ == '__main__':
