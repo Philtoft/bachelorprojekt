a538f05b14fb15ec8be8ebbcedb7fbaf482309fc
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-04-13 10:33:16 +0200

    feat: gets first 10 links

--- trainer-backend/main.py
+++ trainer-backend/main.py
@@ -10,6 +10,7 @@ ws.lang("en")
 app = Flask(__name__)
 CORS(app)
 
+# Should return link with the first 10 articles from the search that can be scraped
 @app.route('/')
 def index():
     wikiURL = request.args.get('url')
@@ -18,17 +19,33 @@ def index():
         return Response('{"error": "No URL provided"}', status=400, mimetype='application/json')
     
     # Check if article exists
-    wiki_article_scraped = get_wiki_data(wikiURL)
+    wiki_article_scraped = get_first_10_links(wikiURL)
     
     return json.dumps({ 'article': wiki_article_scraped })
 
-def get_wiki_data(url):
+def get_first_10_links(url):
     
     response = requests.get(url=url)
 
     soup = bs4(response.content, 'html.parser')
 
     title = soup.find(id="firstHeading")
+
+    # Get first 10 links
+    # remove sidebar table
+    for element in soup.find(class_="sidebar sidebar-collapse nomobile nowraplinks hlist"):
+        element.decompose()
+    
+    first_links = soup.find(id="mw-content-text").find_all("a")
+
+    links = []
+
+    for link in first_links:
+        # Ignores everything in the class "hatnote navigation-not-searchable" and only gets links that start with "/wiki/"
+        if link.find_parent(class_="hatnote navigation-not-searchable") is None and link.has_attr('href') and link['href'].startswith("/wiki/") and len(links) < 10:
+            links.append(link['href'])
+
+    print(f"Links: {links}")
 
     print(title.string)
 
