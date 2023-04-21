c90b00d32c19847e3a67b6b8700f91883471830c
Philip August Hyltoft <philip.hyltoft@gmail.com>
2023-04-21 21:27:40 +0200

    bug: _remove_html_tags doesn't get soup as parameter yet

--- parsing/note_parser.py
+++ parsing/note_parser.py
@@ -1,5 +1,9 @@
+import html
+import re
 from bs4 import BeautifulSoup as bs
 import pathlib
+
+import markdown
 
 _NOTE_FORMATS = [".md", ".html"]
 _NOTE_DIR = "data/notes"
@@ -22,6 +26,7 @@ class NoteParser:
 
     def __call__(self):
         plaintext = self._parse_note()
+        return plaintext
 
     def _parse_note(self) -> str:
         note_format = self._get_note_format()
@@ -34,12 +39,30 @@ class NoteParser:
         
         return self._html_to_plaintext(note)
 
+    def _markdown_to_html(self, markdown_notes: str) -> str:
+        """Converts a markdown string to HTML."""
+
+        escaped_markdown = html.escape(markdown_notes)
+
+        return markdown(escaped_markdown)
 
     def _html_to_plaintext(self, html_notes: str):
         """Converts a """
 
+        soup = bs(html_notes, "html.parser")
+
+         # Clean up h-tags
+        for h_tag in soup.find_all(["h1", "h2", "h3"]):
+            h_tag.string = self._add_colon_if_last_char_not_dot_or_colon(h_tag.text)
+
         tags_to_remove = ["table", "title"]
         self._remove_html_tags(html_notes, tags_to_remove)
+
+        result = soup.get_text(separator=" ")
+        result = result.replace("\n", " ")
+        result = result.replace("\t", " ")
+        result = re.sub("\s\s+", " ", result)
+        return result
 
 
     def _remove_html_tags(self, html_note: str, *tags: str) -> bs:
